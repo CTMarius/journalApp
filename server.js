29fbe80872720afc77273ec53178990beb33481a
CTMarius <mt.cristea@gmail.com>
2023-10-31 19:33:31 +0100

    fbdf

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/journalapi?retryWrites=true&w=majority'); 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*'); // Replace with the appropriate origin(s)
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
