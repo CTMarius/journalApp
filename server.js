d4cb48adfbad2e8a5a7ca0f8ecb1a18ec7a653a9
CTMarius <mt.cristea@gmail.com>
2023-11-01 19:23:10 +0100

    fgfvvb

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ fs = require("fs");
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/journalapi?retryWrites=true&w=majority');
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // Replace with the appropriate origin(s)
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*'); // Replace with the appropriate origin(s)
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 app.use(bodyParser.urlencoded({
   extended: true
