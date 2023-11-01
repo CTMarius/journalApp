3eff3d416cfcdaa061a5f8d10e4cfa9f0647fabe
CTMarius <mt.cristea@gmail.com>
2023-11-01 19:21:55 +0100

    m,nb,knjhm

--- server.js
+++ server.js
@@ -4,20 +4,22 @@ var express = require('express'),
   mongoose = require('mongoose'),
   Task = require('./api/models/journalModel'), //created model loading here
   bodyParser = require('body-parser');
-  https = require("https");
-  fs = require("fs");
-   
+https = require("https");
+fs = require("fs");
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/journalapi?retryWrites=true&w=majority'); 
+mongoose.connect('mongodb://localhost:27017/journalapi?retryWrites=true&w=majority');
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*'); // Replace with the appropriate origin(s)
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 
@@ -25,14 +27,8 @@ var routes = require('./api/routes/journalRoutes'); //importing route
 routes(app); //register the route
 
 
-https
-  .createServer(
-    {
-      key: fs.readFileSync("server.key"),
-      cert: fs.readFileSync("server.cert"),
-    },
-    app
-  ).listen(port, 'localhost', () => {
+app.
+listen(port, 'localhost', () => {
   console.log(`API Server is running on port ${port}`);
 });
 
